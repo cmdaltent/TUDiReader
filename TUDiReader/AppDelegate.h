1d6fd9d190bc5f7733a7e9203aa654c69f15d69f
cmdaltent <weissbach.martin@gmail.com>
2014-10-12 08:23:23 +0200

    Initial Commit WS 15/15

---
+++ TUDiReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TUDiReader
+//
+//  Created by Martin Weissbach on 10/12/14.
+//  Copyright (c) 2014 Martin Weissbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TUDiReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TUDiReader
+//
+//  Created by Martin Weissbach on 10/12/14.
+//  Copyright (c) 2014 Martin Weissbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
